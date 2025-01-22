f1cafb9ab68db479c33d1ce2d3cf1dd92a0a6915
Gustavo <gustavostolzedeandrade@gmail.com>
2025-01-22 09:23:10 -0300

    project created

---
+++ src/test/java/com/gustavostolze/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavostolze.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
